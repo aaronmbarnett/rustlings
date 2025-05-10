73831dbd882afc44108c123094cb5db8d1190005
Aaron Barnett <aaronbarnett@Aarons-Mac-mini.local>
2025-05-10 15:53:12 -0500

    completed modules and hashmaps

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let num_fruit = basket.get(&fruit).copied().unwrap_or(1);
+        basket.entry(fruit).or_insert(num_fruit);
     }
 }
 
